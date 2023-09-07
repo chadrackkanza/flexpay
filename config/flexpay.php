8e6c42c8cf658ddbfcb17b63b2fc9800ef6f13f4
donmbelembe <donmbelembe@users.noreply.github.com>
2023-09-07 18:14:08 +0100

    Update flexpay.php

--- config/flexpay.php
+++ config/flexpay.php
@@ -4,7 +4,7 @@
 return [
     'token' => env('FLEXPAY_TOKEN'),
     'merchant' => env('FLEXPAY_MERCHANT'),
-    'url_api' => env('FLEXPAY_URL_API','http://41.243.7.46:3006'),
+    'url_api' => env('FLEXPAY_URL_API','https://beta-backend.flexpay.cd'),
     'url_api_card' => env('FLEXPAY_URL_API_CARD','https://beta-cardpayment.flexpay.cd'),
     'url_c2b' => '/api/rest/v1/paymentService',
     'url_b2c' => '/api/rest/v1/merchantPayOutService',
